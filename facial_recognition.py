e55a025aae08a2d60763cbb0da8ecc13769f130d
S.T.Santhosh Kumar <69844462+s160785@users.noreply.github.com>
2021-08-17 18:35:11 +0530

    Update facial_recognition.py

--- facial_recognition.py
+++ facial_recognition.py
@@ -1,7 +1,9 @@
+#importing Modules
 import numpy as np
 import cv2
 import face_recognition as fr
 
+#Load Image1
 img = fr.load_image_file("resources\el5n9i.jpg")
 img = cv2.resize(img,(320,640))
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
@@ -10,6 +12,7 @@ encodeimg = fr.face_encodings(img)[0]
 print(faceLoc)
 cv2.rectangle(img,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
 
+#Load image2
 img2 = fr.load_image_file("resources\Malvika.jpg")
 img2 = cv2.resize(img2,(640,320))
 img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2RGB)
@@ -18,6 +21,7 @@ encodeimg2 = fr.face_encodings(img2)[0]
 print(faceLoc2)
 cv2.rectangle(img2,(faceLoc2[3],faceLoc2[0]),(faceLoc2[1],faceLoc2[2]),(255,0,255),2)
 
+#Load Image3
 img3 = fr.load_image_file("resources\malavika-sharma-4.jpg")
 img3 = cv2.resize(img3,(320,640))
 img3 = cv2.cvtColor(img3,cv2.COLOR_BGR2RGB)
@@ -26,6 +30,7 @@ encodeimg3 = fr.face_encodings(img3)[0]
 print(faceLoc3)
 cv2.rectangle(img3,(faceLoc3[3],faceLoc3[0]),(faceLoc3[1],faceLoc3[2]),(255,0,255),2)
 
+#Load Testing Image
 img_Test = fr.load_image_file("resources\Malvika-Sharma-2019.jpg")
 img_Test = cv2.resize(img_Test,(640,320))
 img_Test = cv2.cvtColor(img_Test,cv2.COLOR_BGR2RGB)
@@ -34,6 +39,7 @@ encodeimg_Test = fr.face_encodings(img_Test)[0]
 print(faceLocTest)
 cv2.rectangle(img_Test,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
 
+#Results
 results = fr.compare_faces([encodeimg,encodeimg2,encodeimg3],encodeimg_Test)
 faceDis = fr.face_distance([encodeimg,encodeimg2,encodeimg3],encodeimg_Test)
 print(results,faceDis)
